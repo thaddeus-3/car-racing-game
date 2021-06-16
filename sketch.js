8ed701405e1460a3cdaaf2318d542bb7ff6d4dab
thaddeus-3 <74323070+thaddeus-3@users.noreply.github.com>
2021-06-16 14:46:23 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var database;
+var form, player, game;
+var gameState = 0;
+var playerCount = 0;
+var bg;
+var allPlayers
+var car1, car2, car3, car4;
+var cars
+var carimage1, carimage2, carimage3, carimage4, track;
+var finishedPlayers = 0;
+var finished=false
+var goldimg, silverimg, bronzeimg;
+function preload() {
+    bg = loadImage("carracinggame.jpeg")
+    carimage1 = loadImage("car1.png")
+    carimage2 = loadImage("car2.png")
+    carimage3 = loadImage("car3.png")
+    carimage4 = loadImage("car4.png")
+    track = loadImage("track.jpeg")
+    goldimg=loadImage("gold.png")
+    silverimg=loadImage("silver.png")
+    bronzeimg=loadImage("bronze.png")
+}
+function setup() {
+    createCanvas(displayWidth,displayHeight);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw() {
+     background(bg);
+     if(playerCount===4&& finishedPlayers===0) {
+         game.update(1)
+     }
+     if(gameState===1) {
+         clear()
+         game.play()
+     }
+     if(finishedPlayers===4){
+         game.update(2)
+     }
+     if(gameState===2&& finishedPlayers===4) {
+         game.end()
+     }
+}
